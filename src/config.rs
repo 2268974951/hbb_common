38719617d00820ca90b216a7fdf95a80e1f755c6
weiyi <2268974951@qq.com>
2025-03-20 20:03:33 +0800

    修改中继服务器域名和KEY

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["yi766.vip"];
+pub const RS_PUB_KEY: &str = "QSTy7eWi1wKGkyD6X1rbPROH28ry4ZHDjTaWzlncvGY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
